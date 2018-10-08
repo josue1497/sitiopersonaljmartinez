10de23762e5697f62de281807a27f0fd7b1d9157
Josue Martinez <josuermartinezm@gmail.com>
2018-10-08 03:29:34 -0400

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyBvN0SphkggCX0MigNBqPqTpv8n9F14CRc",
+      authDomain: "personal-site-jmartinez.firebaseapp.com",
+      databaseURL: "https://personal-site-jmartinez.firebaseio.com",
+      projectId: "personal-site-jmartinez",
+      storageBucket: "personal-site-jmartinez.appspot.com",
+      messagingSenderId: "1090925385275"
+  };
+  firebase.initializeApp(config);
